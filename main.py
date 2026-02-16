8f4904e8463b2f65302376274ad3f5370d091c06
Enzo Akira Sasaki dos Santos <enzo@MacBook-Air-de-Enzo.local>
2026-02-15 23:15:05 -0300

    two exercises done

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import spacy
-import pt_core_news_sm
+from spacy import displacy
+from material import mpbc_3p
+from collections import Counter
 
-spacyPT = pt_core_news_sm.load()
+#Loading Portuguese model into processing variable
+nlp = spacy.load("pt_core_news_sm")
 
+#Inserting charpter's text inside processor function
+doc = nlp(mpbc_3p)
+
+# EXERCISE 1 - Extract the characters mentioned on the 3rd charpter of Machado de Assis' classic.
+print("1st exercise answer ->",  [ent for ent in doc.ents if ent.label_ == "PER"])
+
+# EXERCISE 2 - Excract all pronouns from the same charpter. Extra: count how many times they repeat.
+print("2nd exercise answer ->", list(Counter(token.text for token in doc if token.pos_ == "PRON").items()))
+
+# EXERCISE 3 - Display a Dependency Tree of a random sentence from the charpter. Plus: customize visualizers settings.
